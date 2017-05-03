50cf1c34a5aecb58e179d9289e5241ad1477766d
Angel <724727508@qq.com>
2017-05-03 09:40:11 +0800

    about slipelayout bug

--- app/src/main/java/sw/angel/swpullrecyclerlayout/NumAdapter.java
+++ app/src/main/java/sw/angel/swpullrecyclerlayout/NumAdapter.java
@@ -1,9 +1,14 @@
 package sw.angel.swpullrecyclerlayout;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.angel.adapter.SWRecyclerAdapter;
 import com.angel.adapter.SWViewHolder;
-import com.angel.layout.SwipeLinearLayout;
+import com.angel.interfaces.OnSwipeStatusListener;
+import com.angel.layout.SWSlipeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +16,12 @@ import java.util.List;
 /**
  * Created by Angel on 2017/4/7.
  */
-public class NumAdapter extends SWRecyclerAdapter<String> implements SwipeLinearLayout.OnSwipeListener{
+public class NumAdapter extends SWRecyclerAdapter<String> implements OnSwipeStatusListener {
 
     private List<String> list;
     private Context context;
-    List<SwipeLinearLayout> swipeLinearLayouts = new ArrayList<>();
+    private SWSlipeLayout swSlipeLayout;
+    private List<SWSlipeLayout> swSlipeLayouts = new ArrayList<>();
 
     public NumAdapter(Context context, List<String> list) {
         super(context, list);
@@ -24,35 +30,29 @@ public class NumAdapter extends SWRecyclerAdapter<String> implements SwipeLinear
     }
 
     @Override
-    public int getItemLayoutId(int viewType) {
+    public int getItemLayoutId(int layoutID) {
         return R.layout.item;
     }
 
     @Override
-    public void bindData(SWViewHolder holder, int position, String item) {
-        holder.getTextView(R.id.text).setText(list.get(position)+"");
-        swipeLinearLayouts.add(holder.getSWSlipLayout(R.id.sll));
-        holder.getSWSlipLayout(R.id.sll).setOnSwipeListener(this);
-        holder.getSWSlipLayout(R.id.sll).scrollTo(0,0);
+    public void bindData(SWViewHolder holder, final int position, String item) {
+        TextView textView = holder.getTextView(R.id.text);
+        swSlipeLayout = (SWSlipeLayout) holder.getView(R.id.sll);
+        textView.setText(list.get(position) + "");
+        swSlipeLayouts.add(holder.getSWSlipLayout(R.id.sll));
+        textView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(context, position + 1 + "", 1000).show();
+            }
+        });
+        swSlipeLayout.setOnSwipeStatusListener(this);
     }
 
-    @Override
-    public void onDirectionJudged(SwipeLinearLayout thisSll, boolean isHorizontal) {
-        if (false == isHorizontal) {
-            for (SwipeLinearLayout sll : swipeLinearLayouts) {
-                if (null == sll) {
-                    continue;
-                }
-                sll.scrollAuto(SwipeLinearLayout.DIRECTION_SHRINK);
-            }
-        } else {
-            for (SwipeLinearLayout sll : swipeLinearLayouts) {
-                if (null == sll) {
-                    continue;
-                }
-                if (!sll.equals(thisSll)) {
-                    //划开一个sll， 其他收缩
-                    sll.scrollAuto(SwipeLinearLayout.DIRECTION_SHRINK);
+    public void onStatusChanged(SWSlipeLayout layout, SWSlipeLayout.Status status) {
+        if (status == SWSlipeLayout.Status.Open) {
+            for (SWSlipeLayout swSlipeLayout : swSlipeLayouts) {
+                if (swSlipeLayout != layout) {
+                    swSlipeLayout.close();
                 }
             }
         }
