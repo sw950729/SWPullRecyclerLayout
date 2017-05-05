c1719c8b1cd410d4aaaf2b22f47018bce876139d
Angel <724727508@qq.com>
2017-05-05 14:40:15 +0800

    update about swslipelayout

--- app/src/main/java/sw/angel/swpullrecyclerlayout/NumAdapter.java
+++ app/src/main/java/sw/angel/swpullrecyclerlayout/NumAdapter.java
@@ -1,27 +1,24 @@
 package sw.angel.swpullrecyclerlayout;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.angel.adapter.SWRecyclerAdapter;
 import com.angel.adapter.SWViewHolder;
-import com.angel.interfaces.OnSwipeStatusListener;
 import com.angel.layout.SWSlipeLayout;
+import com.angel.utils.SWSlipeManager;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Angel on 2017/4/7.
  */
-public class NumAdapter extends SWRecyclerAdapter<String> implements OnSwipeStatusListener {
+public class NumAdapter extends SWRecyclerAdapter<String> {
 
     private List<String> list;
     private Context context;
     private SWSlipeLayout swSlipeLayout;
-    private List<SWSlipeLayout> swSlipeLayouts = new ArrayList<>();
 
     public NumAdapter(Context context, List<String> list) {
         super(context, list);
@@ -39,28 +36,15 @@ public class NumAdapter extends SWRecyclerAdapter<String> implements OnSwipeStat
         TextView textView = holder.getTextView(R.id.text);
         swSlipeLayout = (SWSlipeLayout) holder.getView(R.id.sll);
         textView.setText(list.get(position) + "");
-        swSlipeLayouts.add(holder.getSWSlipLayout(R.id.sll));
         textView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (swSlipeLayout.isOpen()) {
-                    for (SWSlipeLayout swSlipeLayout : swSlipeLayouts) {
-                        swSlipeLayout.close();
-                    }
+                if (SWSlipeManager.getInstance().haveOpened()) {
+                    SWSlipeManager.getInstance().close();
                 } else {
                     Toast.makeText(context, position + 1 + "", 1000).show();
                 }
             }
         });
-        swSlipeLayout.setOnSwipeStatusListener(this);
     }
 
-    public void onStatusChanged(SWSlipeLayout layout, SWSlipeLayout.Status status) {
-        if (status == SWSlipeLayout.Status.Open) {
-            for (SWSlipeLayout swSlipeLayout : swSlipeLayouts) {
-                if (swSlipeLayout != layout) {
-                    swSlipeLayout.close();
-                }
-            }
-        }
-    }
 }
